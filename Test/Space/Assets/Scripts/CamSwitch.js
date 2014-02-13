6428395ba6d2f2f840ded4d18e61717c64d4106a
Zachary Gauld <zg0789@gmail.com>
2014-02-13 11:02:17 -0700

    Added first person camera

---
+++ Test/Space/Assets/Scripts/CamSwitch.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+public var firstPerson : Camera;
+public var thirdPerson : Camera;
+
+function Update ()
+{
+	if (Input.GetKey(KeyCode.F1))
+	{
+		firstPerson.camera.enabled = true;
+		thirdPerson.camera.enabled = false;
+	}
+	
+	if (Input.GetKey(KeyCode.F2))
+	{
+		firstPerson.camera.enabled = false;
+		thirdPerson.camera.enabled = true;
+	}
+}

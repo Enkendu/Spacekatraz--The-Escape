001d70007ea1f675665aae228cfb7dc059fe2bc9
njkloos <njkloosmonster@gmail.com>
2014-02-22 16:08:40 -0500

    Boss Ship
    Update to boss ship

---
+++ Test/Space/Assets/Scripts/BossFiring.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (AudioClip))]
+public class BossFiring : MonoBehaviour {
+	
+	// Variables used to target the player
+	private GameObject player;
+	private int viewDistance = 300;
+	private float gapDistance;
+	
+	// weapon fire variables
+	public GameObject projectile;
+	public float fireRate = 0.5f;
+	float nextFire;
+	public float speed = 50.0f;
+	Material material;
+	public float damage = 10;
+	
+	//sound for laser fire
+	public AudioClip laserSound;
+	
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("playerShip");
+		nextFire = Time.time;
+		material = new Material(Shader.Find("Diffuse"));	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(player != null)
+		{
+			gapDistance = Vector3.Distance (this.transform.position, player.transform.position);
+			
+			if (gapDistance <= viewDistance) 
+			{
+
+				if(LevelController.canFire == true && LevelController.pause == false)
+				{
+					fireWeapon();
+				}
+			}
+		}
+		
+	}
+	
+	void fireWeapon()
+	{
+		if (Time.time > nextFire) 
+		{
+			nextFire = Time.time + fireRate;
+			GameObject newClone = (GameObject)Instantiate(projectile,this.transform.position, transform.rotation);
+			newClone.transform.position = newClone.transform.position-(newClone.transform.forward+newClone.transform.localScale);
+			Physics.IgnoreCollision (newClone.collider, transform.collider);
+			Physics.IgnoreCollision (newClone.collider, transform.parent.collider);
+			newClone.transform.localScale = newClone.transform.localScale*2;
+			material.color = new Color (1, 0, 1, 0);
+			newClone.renderer.material = material;
+			newClone.rigidbody.velocity = transform.TransformDirection(transform.forward*-speed);
+			
+			audio.PlayOneShot(laserSound, 1.0f);
+			
+		}
+	}
+}
